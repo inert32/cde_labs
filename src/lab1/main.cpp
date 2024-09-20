957132b25545ef3b10b8297a93d8589c1c3a3df9
Михайлов Алексей <inert32@gmail.com>
2024-09-20 16:10:39 +0500

    refactor(lab1): add 'methods_base' interface feat(lab1): add method selection from CLI build: raise project version

--- src/lab1/main.cpp
+++ src/lab1/main.cpp
@@ -38,25 +38,32 @@ public:
 };
 
 // Методы
+class methods_base {
+public:
+    virtual double calc(const functions* fn, const size_t count, const double from, const double to) const = 0;
+};
+
 // Метод Монте-Карло
-double monte_carlo(const functions* fn, const size_t count, const double from, const double to) {
-    std::cout << "Using Monte-Carlo method" << std::endl;
+class monte_carlo : public methods_base {
+public:
+    double calc(const functions* fn, const size_t count, const double from, const double to) const {
+        std::cout << "Using Monte-Carlo method" << std::endl;
 
-    // Запуск генератора
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution nums(from, to);
+        // Запуск генератора
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution nums(from, to);
 
-    // Расчет попаданий
-    double sum = 0;
-    for (size_t i = 0; i < count; i++) {
-        sum = sum + fn->calc(nums(gen));
+        // Расчет попаданий
+        double sum = 0;
+        for (size_t i = 0; i < count; i++)
+            sum = sum + fn->calc(nums(gen));
+
+        const double mult = (to - from) / (double)count;
+
+        return sum * mult;
     }
-
-    const double mult = (to - from) / (double)count;
-
-    return sum * mult;
-}
+};
 
 int main(int argc, char** argv) {
     std::cout << "Lab1 " << build_version << " " << build_git << std::endl;
@@ -64,7 +71,7 @@ int main(int argc, char** argv) {
     // Разбор параметров
     size_t iter_count = 1000;
     double from = 0, to = 1;
-    functions* fn;
+    functions* fn; methods_base* method;
 
     const auto cli = parse_cli(argc, argv);
     auto it = cli.find("count");
@@ -86,6 +93,13 @@ int main(int argc, char** argv) {
         }
     } else fn = new func4;
 
-    std::cout << monte_carlo(fn, iter_count, from, to) << std::endl;
+    // Выбор метода
+    it = cli.find("func");
+    if (it != cli.end()) {
+        if (it->second == "mt") method = new monte_carlo;
+        else method = new monte_carlo;
+    } else method = new monte_carlo;
+
+    std::cout << method->calc(fn, iter_count, from, to) << std::endl;
     return 0;
 }
