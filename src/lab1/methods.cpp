53456f5b933198e149f4431097abb10af1a96c5d
Михайлов Алексей <inert32@gmail.com>
2024-09-20 20:27:35 +0500

    feat(lab1): add Simpson's rule build: drop version before merge with main

--- src/lab1/methods.cpp
+++ src/lab1/methods.cpp
@@ -47,12 +47,31 @@ double trapezoid::calc(const func_base* fn, const size_t count, const double fro
     return step * (sum + borders);
 }
 
+double simpson::calc(const func_base* fn, const size_t count, const double from, const double to) const {
+    std::cout << "Using Simpson's method" << std::endl;
+
+    const size_t count_n = count * 2;
+    const double step = (to - from) / count_n;
+
+    double sum1 = 0.0, sum2 = 0.0;
+    for (size_t i = 1; i < count_n; i+=2) {
+        sum1 = sum1 + fn->calc(from + step * i); // Проход по нечетным членам
+        sum2 = sum2 + fn->calc(from + step * (i + 1)); // Проход по четным членам
+    }
+
+    sum1 = sum1 * 4.0;
+    sum2 = sum2 * 2.0;
+
+    return step / 3.0 * (fn->calc(from) + fn->calc(to) + sum1 + sum2);
+}
+
 methods_base* method_select(const cli_map& cli, [[maybe_unused]] const bool threads) {
     try {
         const auto name = cli.at("method");
 
         if (name == "quad") return new quadrants;
         else if (name == "trapez") return new trapezoid;
+        else if (name == "simpson") return new simpson;
         else throw std::exception();
     }
     catch (const std::exception&) {
