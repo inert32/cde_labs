334fc1edd8da13115e4b7547376644253c39f046
Михайлов Алексей <inert32@gmail.com>
2024-10-14 14:07:55 +0500

    feat(lab3): add task file parser
    So far parser knows only 'area' and 'source' commands.

--- src/lab3/main.cpp
+++ src/lab3/main.cpp
@@ -2,10 +2,51 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <iostream>
+#include <fstream>
 #include "../build.h"
 #include "../common.h"
 
+// Парсер файла задания
+cli_map parse_task_file(const std::string& path) {
+    std::ifstream file(path);
+    if (!file.good()) throw std::runtime_error("Failed to read task file");
+
+    std::string line, command, args;
+    cli_map ret;
+    while (std::getline(file, line)) {
+        if (line.length() == 0 || line[0] == '#') continue; // Пропускаем пустые строки и комментарии
+
+        auto split = line.find(' ');
+        if (split == line.npos) {
+            command = line;
+            args.clear();
+        }
+        else {
+            command = line.substr(0, split);
+            args = line.substr(split + 1, line.length());
+        }
+
+        // Обработка команд
+        if (command == "area" || command == "source") ret["area"] = args;
+        else std::cout << "parse: Unknown command: " << command << std::endl;
+    }
+
+    return ret;
+}
+
 int main(int argc, char** argv) {
-    std::cout << "Lab2 " << build_version << " " << build_git << std::endl;
+    std::cout << "Lab3 " << build_version << " " << build_git << std::endl;
+    auto cli = parse_cli(argc, argv);
+
+    try {
+        const std::string task_file = str_from_cli_map(cli, "file");
+        auto conf = parse_task_file(task_file);
+        std::cout << "Task file parsed." << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cout << "parse: " << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
