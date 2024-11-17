bdb770597f086ec1c4850d6eb5ed09bcfff372a7
Михайлов Алексей <inert32@gmail.com>
2024-11-17 21:16:25 +0500

    build(lab4): start work on lab4

---
+++ src/lab4/main.cpp
@@ -0,0 +1,14 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <iostream>
+#include "../build.h"
+#include "../common.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Lab4 " << build_version << " " << build_git << std::endl;
+    auto cli = parse_cli(argc, argv);
+    const bool run_sdl = bool_from_cli_map(cli, "graph", true);
+
+    return 0;
+}
