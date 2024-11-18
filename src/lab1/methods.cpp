19f2e0b204a880f2495ff54b6642f6e383602400
Михайлов Алексей <inert32@gmail.com>
2024-11-18 21:05:48 +0500

    style: add GPL3 header to all source files build: ignore V1042 in PVS-Studio

--- src/lab1/methods.cpp
+++ src/lab1/methods.cpp
@@ -1,3 +1,20 @@
+/*
+Laboratory work on numerical methods for solving convection-diffusion equations
+Copyright (C) 2024 Mikhaylov Alexey (inert32@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
